ef4d20c5015d6d86fc793361bdc3091151423adb
Michael Gurevich <michael.gurevich.02@mail.ru>
2023-12-05 16:25:25 +0300

    feat: move `index.tsx` and add 404 fallback

--- src/app/pages/AppRouter.tsx
+++ src/app/pages/AppRouter.tsx
@@ -6,6 +6,7 @@ export default function AppRouter() {
       <Route index Component={() => <>Hello world!</>} />
       <Route path="/commanders" Component={() => <>Commanders</>} />
       <Route path="/card-choices" Component={() => <>Card choices</>} />
+      <Route path="*" Component={() => <>404</>} />
     </Routes>
   )
 }
