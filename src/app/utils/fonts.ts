6bb46355ec0a346530a048b01814d93203ebcd16
Michael Gurevich <michael.gurevich.02@mail.ru>
2023-12-08 17:33:04 +0300

    feat: add more fonts

--- src/app/utils/fonts.ts
+++ src/app/utils/fonts.ts
@@ -2,6 +2,7 @@ import "~/assets/fonts.css"
 
 const family = {
   header: "SF-Pro-Display-Bold, Roboto, sans-serif",
+  headerMedium: "SF-Pro-Display-Medium, Roboto, sans-serif",
   light: "Outfit-Light, Tahoma, sans-serif",
   regular: "Outfit-Regular, Tahoma, sans-serif",
   medium: "Outfit-Medium, Tahoma, sans-serif",
@@ -9,9 +10,79 @@ const family = {
   bold: "Outfit-Bold, Tahoma, sans-serif",
 }
 
+export const text14 = {
+  fontFamily: family.regular,
+  fontSize: "14px",
+  fontStyle: "normal",
+  lineHeight: "normal",
+}
+
+export const text14Medium = {
+  fontFamily: family.medium,
+  fontSize: "14px",
+  fontStyle: "normal",
+  lineHeight: "normal",
+}
+
+export const text16Medium = {
+  fontFamily: family.medium,
+  fontSize: "16px",
+  fontStyle: "normal",
+  lineHeight: "normal",
+}
+
+export const text20Medium = {
+  fontFamily: family.medium,
+  fontSize: "20px",
+  fontStyle: "normal",
+  lineHeight: "normal",
+}
+
+export const header12Medium = {
+  fontFamily: family.headerMedium,
+  fontSize: "12px",
+  fontStyle: "normal",
+  lineHeight: "normal",
+}
+
+export const header16Medium = {
+  fontFamily: family.headerMedium,
+  fontSize: "16px",
+  fontStyle: "normal",
+  lineHeight: "normal",
+}
+
+export const header20 = {
+  fontFamily: family.header,
+  fontSize: "20px",
+  fontStyle: "normal",
+  lineHeight: "normal",
+}
+
+export const header24 = {
+  fontFamily: family.header,
+  fontSize: "24px",
+  fontStyle: "normal",
+  lineHeight: "normal",
+}
+
 export const header28 = {
   fontFamily: family.header,
   fontSize: "28px",
+  fontStyle: "normal",
+  lineHeight: "normal",
+}
+
+export const header32 = {
+  fontFamily: family.header,
+  fontSize: "32px",
+  fontStyle: "normal",
+  lineHeight: "normal",
+}
+
+export const header52 = {
+  fontFamily: family.header,
+  fontSize: "52px",
   fontStyle: "normal",
   lineHeight: "normal",
 }
